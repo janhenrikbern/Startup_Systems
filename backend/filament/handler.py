35121955a768f80bf695d034a1e08e61c39f51a2
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-04-01 11:07:34 -0400

    Module 4, hw 1, 2 backend

--- backend/filament/handler.py
+++ backend/filament/handler.py
@@ -1,10 +1,26 @@
 import json
+import os
+
+def get_test_data(id):
+    filename = os.path.join('static', 'test.json')
+    with open(filename) as file:
+        data = json.load(file)
+
+    data["fileID"] = int(id)
+    return data
 
 def hello(event, context):
+    path = event["pathParameters"]["proxy"].split('/')
+    print(path)
     if event["path"] == "/whoami" and event["httpMethod"] == "GET":
         body = {
             "message": "jhb353"
         }
+    elif path[0] == "energy" and event["httpMethod"] == "GET":
+        if len(path) > 1:
+            body = get_test_data(path[1])
+        else:
+            body = get_test_data(-1)
     else:
         body = {
             "input": event
