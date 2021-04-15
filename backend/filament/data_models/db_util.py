ea2b568ee43b68a623cde828139e028b71a90b87
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-04-15 17:51:22 -0400

    updated db methods

--- backend/filament/data_models/db_util.py
+++ backend/filament/data_models/db_util.py
@@ -3,23 +3,29 @@ import boto3
 import json
 
 
+def get_db(is_local=True):
+    if is_local:
+        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+    else:
+        dynamodb = boto3.resource('dynamodb')
+
+    return dynamodb
+
 def serialize(instance):
     return json.dumps(vars(instance))
 
-def create_table_entry(instance, dynamodb=None):
+def create_table_entry(instance, local=True):
     """
     PUT method. Adds new entry to data table
     """
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+    dynamodb = get_db()
 
     table = dynamodb.Table(instance.table)
-    response = table.put_item(Item=vars(instance))
+    table.put_item(Item=vars(instance))
     print(f"Added new entry to {instance.table}: {instance.id}")
 
-def get_table_entry(table, id, dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+def get_table_entry(table, id, local=True):
+    dynamodb = get_db()
 
     table = dynamodb.Table(table)
     response = table.get_item(Key={'id': id})
@@ -29,13 +35,11 @@ def get_table_entry(table, id, dynamodb=None):
         print("Instance with given id doesn't exist.")
         return None
 
-def update_table_entry(instance, element, dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
+def update_table_entry(instance, element, local=True):
+    dynamodb = get_db()
         
     table = dynamodb.Table(instance.table)
-    val = instance.__dict__[element]
-    response = table.update_item(
+    table.update_item(
         Key={'id': instance.id},
         UpdateExpression= f'SET {element} = :val1',
         ExpressionAttributeValues={
