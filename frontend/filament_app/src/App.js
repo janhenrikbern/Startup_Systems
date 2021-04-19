9da3b11bc4bf69b6300383df4b5e70dbce65ec44
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-04-19 09:06:00 -0400

    minor cleanup

--- frontend/filament_app/src/App.js
+++ frontend/filament_app/src/App.js
@@ -1,13 +1,6 @@
 import './App.scss';
 import Welcome from 'components/Welcome';
 
-const isLocalEnv = window.location.href.includes('localhost')
-
-let backendUrl = 'https://ulo5y72k4m.execute-api.us-east-1.amazonaws.com/dev'
-if (isLocalEnv) {
-  backendUrl = 'http://localhost:4000/dev'
-}
-
 function App() {
   return (
     <div className="App">
